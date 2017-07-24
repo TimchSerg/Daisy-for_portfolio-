cd0cad6f4ca366bfdeb2eba8ae93e5025adede36
Timch <cool.timch2014@yandex.ru>
2017-07-25 00:54:03 +0300

    Отредоктированы стили и добавлен script для бургер-меню

--- js/common.js
+++ js/common.js
@@ -1,3 +1,17 @@
+$(document).ready(function(){
+    $('.btn-burg').on('click', function(){
+        $('.nav-header ul').slideToggle(500);
+    });
+    $(window).resize(function(){
+        if ($(window).width()>768){
+            $('.nav-header ul').removeAttr('style');
+        }
+    })
+})
+
+
+
+
 $('.nav-header ul li a').on('click', function(e) {
     e.preventDefault(); //Если будет вызван данный метод, то действие события по умолчанию не будет выполнено
     $('.nav-header ul li a').removeClass('active');
