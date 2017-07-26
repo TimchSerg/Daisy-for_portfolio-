a70fd9396e2742c1a9c5705626e778bdf21d8c2d
Timch <cool.timch2014@yandex.ru>
2017-07-26 15:51:03 +0300

    Добавлены новые события в блоке block-offer

--- js/common.js
+++ js/common.js
@@ -7,6 +7,15 @@ $(document).ready(function(){
             $('.nav-header ul').removeAttr('style');
         }
     })
+    
+    //Вешаем события клик на стрелки (навигации)
+    $('.arrow').on('click', 'a', function(e){
+        e.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body, html').animate({scrollTop: top}, 1500);
+       
+    });
 })
 
 
@@ -16,4 +25,17 @@ $('.btn-a-nav').on('click', function(e) {
     e.preventDefault(); //Если будет вызван данный метод, то действие события по умолчанию не будет выполнено
     $('.btn-a-nav').removeClass('active');
     $(this).addClass('active');
+})
+
+$('.btn-prev-offter').on('click', function(e) {
+    e.preventDefault(); //Если будет вызван данный метод, то действие события по умолчанию не будет выполнено
+    var src = $(this).attr('data-img');
+    
+    $('#img-1').attr('src', 'img/icons/clients-NOactive.png');
+    $('#img-2').attr('src', 'img/icons/braiding-NOactive.png');
+    $('#img-3').attr('src', 'img/icons/Marketing-NOactive.png');
+    $('#img-4').attr('src', 'img/icons/ADV-NOactive.png');
+    
+    
+    $(this).attr('src', src);
 })
